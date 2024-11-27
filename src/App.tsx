8da595c5495cb91178d5c101bcbdf0282d313096
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-11-27 03:33:44 -0300

    feat: quotes mock quotes
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import './App.css'
+import { Qoutes } from './feature/quotes/Qoutes'
 import logo from './logo.svg'
 
 const App = () => {
     return (
         <div className="App">
-            <header>
+            <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
+                <h2>React & Redux Toolkit</h2>
             </header>
-            <h1>React App!</h1>
+            <main>
+                <Qoutes />
+            </main>
         </div>
     )
 }
